b767480b958ff87c67255ff86143908fdd24cf0c
Manuel Berger <lunartists@gmail.com>
2022-04-29 16:49:02 +0200

    feat: add eco2 and tvoc to payload
    Signed-off-by: Manuel Berger <lunartists@gmail.com>

--- raspberry/data-collector.py
+++ raspberry/data-collector.py
@@ -3,17 +3,15 @@ import json
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
 from aws_helpers.custom_shadow_helpers import customShadowCallback_Delete, customShadowCallback_Update
 
-
 # from get_sensor_data.sensor_sgp30 import get_sensor_sgp30
 from get_sensor_data.sensor_scd30 import get_sensor_scd30_co2, get_sensor_scd30_temp, get_sensor_scd30_rh
+from get_sensor_data.sensor_sgp30 import get_sensor_sgp30
 from get_sensor_data.sensor_enviro import get_sensor_pms5003
 from get_sensor_data.sensor_enviro import get_sensor_nh3, get_sensor_ox, get_sensor_red
 from get_sensor_data.sensor_zigbee import get_sensor_zigbee
 from get_sensor_data.sensor_tado import get_sensor_heating
 
-
 API_ENDPOINT = "abeqm7ntwv6xv-ats.iot.us-east-1.amazonaws.com"
-
 
 # Init AWSIoTMQTTShadowClient
 myAWSIoTMQTTShadowClient = AWSIoTMQTTShadowClient('PiClient')
@@ -39,48 +37,63 @@ deviceShadowHandler.shadowDelete(customShadowCallback_Delete, 5)
 
 # Read data from moisture sensor and update shadow
 while True:
-    zigbee = get_sensor_zigbee()
-    particles = get_sensor_pms5003()
-    temperature = zigbee["living-room-multi"]["temperature"]/100
-    humidity = zigbee["living-room-multi"]["humidity"]/100
-    co2 = get_sensor_scd30_co2()
-    nh3 = get_sensor_nh3()
-    ox = get_sensor_ox()
-    red = get_sensor_red()
-    pm1 = particles.pm_ug_per_m3(1)
-    pm2 = particles.pm_ug_per_m3(2.5)
-    pm10 = particles.pm_ug_per_m3(10)
-    heating = get_sensor_heating()
-    livingroom_door_open = zigbee["livingroom-door"]["open"]
-    livingroom_window_open = zigbee["livingroom-window"]["open"]
-    balcony_door_open = zigbee["balcony-door"]["open"]
-    kitchen_window_open = zigbee["kitchen-window"]["open"]
-    fallback_temperature = get_sensor_scd30_temp()
-    fallback_humidity = get_sensor_scd30_rh()
+    try:
+        zigbee = get_sensor_zigbee()
+        particles = get_sensor_pms5003()
+        temperature = zigbee["living-room-multi"]["temperature"]/100
+        humidity = zigbee["living-room-multi"]["humidity"]/100
+        pressure = zigbee["living-room-multi"]["pressure"]
+        temperature_outdoor = zigbee["balcony-multi"]["temperature"]/100
+        humidity_outdoor = zigbee["balcony-multi"]["humidity"]/100
+        pressure_outdoor = zigbee["balcony-multi"]["pressure"]
+        co2 = get_sensor_scd30_co2()
+        nh3 = get_sensor_nh3()
+        ox = get_sensor_ox()
+        red = get_sensor_red()
+        pm1 = particles.pm_ug_per_m3(1)
+        pm2 = particles.pm_ug_per_m3(2.5)
+        pm10 = particles.pm_ug_per_m3(10)
+        heating = get_sensor_heating()
+        tvoc = get_sensor_sgp30().total_voc
+        eco2 = get_sensor_sgp30().equivalent_co2
+        livingroom_door_open = zigbee["livingroom-door"]["open"]
+        livingroom_window_open = zigbee["livingroom-window"]["open"]
+        balcony_door_open = zigbee["balcony-door"]["open"]
+        kitchen_window_open = zigbee["kitchen-window"]["open"]
 
-    # Display moisture and temp readings
-    print("Temperature: {}".format(temperature))
-    print("Relative Humidity: {}".format(humidity))
-    print("CO2 Level: {}".format(co2))
-    print("NH3 Level: {}".format(nh3))
-    print("OX Level: {}".format(ox))
-    print("RED Level: {}".format(nh3))
-    print("PM Level: {pm1} PM1, {pm2} PM2.5, {pm10} PM10".format(
-        pm1=pm1, pm2=pm2, pm10=pm10))
-    print("Heating Level: {}".format(heating))
-    print("Livingroom door open: {}".format(livingroom_door_open))
-    print("Livingroom window open: {}".format(livingroom_window_open))
-    print("Balcony door open: {}".format(balcony_door_open))
-    print("Kitchen window open: {}".format(kitchen_window_open))
+        # Display moisture and temp readings
+        print("Temperature: {}".format(temperature))
+        print("Relative Humidity: {}".format(humidity))
+        print("Pressure: {}".format(pressure))
+        print("Temperature Outdoor: {}".format(temperature_outdoor))
+        print("Relative Humidity Outdoor: {}".format(humidity_outdoor))
+        print("Pressure Outdoor: {}".format(pressure_outdoor))
+        print("CO2 Level: {}".format(co2))
+        print("NH3 Level: {}".format(nh3))
+        print("OX Level: {}".format(ox))
+        print("RED Level: {}".format(nh3))
+        print("PM Level: {pm1} PM1, {pm2} PM2.5, {pm10} PM10".format(
+            pm1=pm1, pm2=pm2, pm10=pm10))
+        print("Heating Level: {}".format(heating))
+        print("TVOC Level: {}".format(tvoc))
+        print("eCO2 Level: {}".format(eco2))
+        print("Livingroom door open: {}".format(livingroom_door_open))
+        print("Livingroom window open: {}".format(livingroom_window_open))
+        print("Balcony door open: {}".format(balcony_door_open))
+        print("Kitchen window open: {}".format(kitchen_window_open))
 
-    # Create message payload
-    payload = {"state": {"reported": {
-        "temperature": str(temperature), "humidity": str(humidity), "co2": str(co2), "nh3": str(nh3), "ox": str(ox),
-        "red": str(red), "pm1": str(pm1), "pm2": str(pm2), "pm10": str(pm10), "heating": str(heating),
-        "livingroom_door_open": str(livingroom_door_open), "livingroom_window_open": str(livingroom_window_open), "balcony_door_open": str(balcony_door_open),
-        "kitchen_window_open": str(kitchen_window_open), "fallback_temperature": str(fallback_temperature), "fallback_humidity": str(fallback_humidity), }}}
+        # Create message payload
+        payload = {"state": {"reported": {
+            "temperature": temperature, "humidity": humidity, "pressure": pressure, "temperature_outdoor": temperature_outdoor,
+            "humidity_outdoor": humidity_outdoor, "pressure_outdoor": pressure_outdoor, "co2": co2, "nh3": nh3, "ox": ox,
+            "red": red, "pm1": pm1, "pm2": pm2, "pm10": pm10, "heating": heating, "tvoc": tvoc, "eco2": eco2,
+            "livingroom_door_open": livingroom_door_open, "livingroom_window_open": livingroom_window_open, "balcony_door_open": balcony_door_open,
+            "kitchen_window_open": kitchen_window_open}}}
 
-    # Update shadow
-    deviceShadowHandler.shadowUpdate(json.dumps(
-        payload), customShadowCallback_Update, 5)
+        # Update shadow
+        deviceShadowHandler.shadowUpdate(json.dumps(
+            payload), customShadowCallback_Update, 5)
+    except ValueError:
+        print("Something went wrong while trying to create the payload")
+
     time.sleep(10)
