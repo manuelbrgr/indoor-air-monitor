2b274a76c3f5a9b3e3482808e1d8dc9e1813288b
Manuel Berger <lunartists@gmail.com>
2022-05-09 20:41:08 +0200

    feat: fix issue with pms sensor
    Signed-off-by: Manuel Berger <lunartists@gmail.com>

--- raspberry/get_sensor_data/sensor_enviro.py
+++ raspberry/get_sensor_data/sensor_enviro.py
@@ -1,7 +1,14 @@
 from pms5003 import PMS5003, ReadTimeoutError
 from enviroplus import gas
 
-pm_sensor = PMS5003()
+
+pm_sensor = PMS5003(
+    device='/dev/ttyAMA0',
+    baudrate=9600,
+    pin_enable=22,
+    pin_reset=27
+)
+
 readings = False
 
 
@@ -9,8 +16,13 @@ def get_sensor_pms5003():
     global pm_sensor
     try:
         readings = pm_sensor.read()
-    except ReadTimeoutError:
-        pm_sensor = PMS5003()
+    except:
+        pm_sensor = PMS5003(
+            device='/dev/ttyAMA0',
+            baudrate=9600,
+            pin_enable=22,
+            pin_reset=27
+        )
         readings = pm_sensor.read()
     return readings
 
---
+++ raspberry/test.py
@@ -0,0 +1,10 @@
+import time
+
+while True:
+    try:
+        0/0
+        print(1)
+    except:
+        print(0)
+
+    time.sleep(2)
